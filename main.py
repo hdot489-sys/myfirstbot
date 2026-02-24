57b629c83b4745c5daff67c414509779b68e9435
hdot489-sys <hdot489@gmail.com>
2026-02-24 15:14:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ async def joke(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Ek joke suno 😄\nTeacher: Homework kyu nahi kiya?\nStudent: Sir network issue tha 😂")
 
 if "tum batao" in text:
-    await update.massage.reply_text("Mai badhiya tum batao ")
-else:
+    await update.massage.reply_text("Mai badhiya tum batao")
+if "baigan" in text:
     await update.massage.reply_text("Jyada Juban Mat chalao")
 
 # Commands
@@ -66,6 +66,7 @@ if __name__ == '__main__':
 
     print("Bot is starting... ✅")
     app.run_polling()
+
 
 
 
